573c275ee33c81d9b10219919e9e45c82f2f9ee2
nikita takne <nikitatakne22@gmail.com>
2024-03-11 00:15:47 +0530

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
+// decorator function in Angular used to define the metadata 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+// handle any additional initialization tasks - Oninit
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() { }  //dependency injection
 
   ngOnInit(): void {
   }
